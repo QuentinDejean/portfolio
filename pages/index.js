c61b2de5bed702eb52f8d170a81704d47357d181
Quentin Dejean <quentin@cammy.com>
2016-11-12 08:01:20 +1100

    Added basic express server config

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Head  from 'next/head';
-import MainLayout from '../src/components/layout/Main';
-import React from 'React';
-
-export default () => (
-  <MainLayout>
-    <p>Hello world!</p>
-  </MainLayout>
-);

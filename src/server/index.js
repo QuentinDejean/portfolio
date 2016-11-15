3a2ba3f0019e3325ee9274fb7408fe3a8eb66a3f
Quentin Dejean <quentin@cammy.com>
2016-11-16 10:37:15 +1100

    Added working express server with health endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const healthController = require('./routes/health');
+
 const app = express();
 app.disable('x-powered-by');
 
@@ -10,5 +12,6 @@ app.use(express.static(`${__dirname}/../client/assets`, {
 
 app.use(morgan('dev'));
 
+app.use('/health', healthController);
 
-export default express;
+module.exports = app;
